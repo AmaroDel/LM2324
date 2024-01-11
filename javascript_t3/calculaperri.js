ba4073ffeaa9b820f6c7d6d37052ae8a715b4145
Amaro del Águila Salas <amarodelaguila04@iesamachado.org>
2024-01-11 12:42:25 +0100

    a

---
+++ javascript_t3/calculaperri.js
@@ -0,0 +1,23 @@
+function suma(){
+    var n1 = parseInt(document.getElementById("n1").value);
+    var n2 = parseInt(document.getElementById("n2").value);
+    document.getElementById("resultado").innerHTML = n1+n2;
+}
+
+function resta(){
+    var n1 = parseInt(document.getElementById("n1").value);
+    var n2 = parseInt(document.getElementById("n2").value);
+    document.getElementById("resultado").innerHTML = n1-n2;
+}
+
+function multi(){
+    var n1 = parseInt(document.getElementById("n1").value);
+    var n2 = parseInt(document.getElementById("n2").value);
+    document.getElementById("resultado").innerHTML = n1*n2;
+}
+
+function divide(){
+    var n1 = parseInt(document.getElementById("n1").value);
+    var n2 = parseInt(document.getElementById("n2").value);
+    document.getElementById("resultado").innerHTML = n1/n2;
+}
