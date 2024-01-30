82623875fe330915fe27f57b012eb2a002b106a2
Amaro Del Águila Salas <amarodelaguila04@iesamachado.org>
2024-01-30 18:04:26 +0100

    a

---
+++ javascript_t3/ejercicios/imagenrandom.js
@@ -0,0 +1,24 @@
+function imagen(){
+    var Imagen = [
+        {
+            src: "../images/arbolverde.jpg"
+        },
+        {
+            src: "../images/arbolamarillo.jpg"
+        },
+        {
+            src: "../images/arbolotono.jpg"
+        },
+        {
+            src: "../images/arbolnevado.jpg"
+        },
+        {
+            src: "../images/arbolrosa.jpg"
+        }
+    ];
+
+    var Eleccion = []
+
+    
+
+}
