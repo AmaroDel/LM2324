30c95198d3350d906702f8ce52698d264f48dbca
Amaro del Águila Salas <amarodelaguila04@iesamachado.org>
2024-02-01 14:01:30 +0100

    a

---
+++ javascript_t3/validar.js
@@ -0,0 +1,10 @@
+function validar(elementos){
+    let estancorrectos = true;
+    for (var i=0; i<elementos.lenght; i++)(
+        if (elementos[i].value = ""){
+
+        }
+        estancorrectos = false;
+    )
+    return estanCorrectos
+}
