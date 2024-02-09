4f0dc1640745b4b8805af341f44806a496b74ee5
Amaro del Águila Salas <amarodelaguila04@iesamachado.org>
2024-02-09 12:39:41 +0100

    a

--- javascript_t3/mayor.js
+++ javascript_t3/mayor.js
@@ -1,29 +1,9 @@
-var vector =[]
-
-function anadir() {
-    var numero = parseFloat(document.getElementById("elemento").value);
-
-    //Borramos cualquier mensaje de error previo
-    document.getElementById("aviso").innerHTML= ""
-
-    if (isNaN(numero)){  //Si true significa que no es un número
-        document.getElementById("aviso").innerHTML= "Debes introducir un número"
-    }
-    else{
-    vector.push(numero);
-    document.getElementById("mostrar").innerHTML = vector
-    }
-}
-
-function mayor(){
+function mayor(vector){
     let mayor=vector[0]; //por defecto el primer elemento es el mayor
-    for (var i=0; i<vector.length;i++){
+    for (var i=0; i<vector.lenght;i++){
         if (vector[i]>mayor)
         mayor = vector[i];
 
     }
-    return document.getElementById("mostrar").innerHTML = mayor;
-  
-    
-    
+    return mayor;
 }
