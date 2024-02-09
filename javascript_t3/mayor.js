89398588a06e4cf817365556b73bad6edb305811
Amaro del Águila Salas <amarodelaguila04@iesamachado.org>
2024-02-09 12:55:32 +0100

    a

--- javascript_t3/mayor.js
+++ javascript_t3/mayor.js
@@ -1,6 +1,6 @@
 function mayor(vector){
     let mayor=vector[0]; //por defecto el primer elemento es el mayor
-    for (var i=0; i<vector.lenght;i++){
+    for (var i=0; i<vector.length;i++){
         if (vector[i]>mayor)
         mayor = vector[i];
 
